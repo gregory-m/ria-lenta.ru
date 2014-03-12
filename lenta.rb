7233af34c7f5cb6fc5f234fac441bb7605b6072c
Gregory Man <man.gregory@gmail.com>
2014-03-12 19:36:44 +0200

    Implemnted rubrics controller

--- lenta.rb
+++ lenta.rb
@@ -5,13 +5,27 @@ require './feeds_updater'
 set :cache, Dalli::Client.new
 FeedsUpdater.update
 
+set :feeds_names, ['politics', 'world', 'economy', 'science', 'sport', 'culture', 'eco']
+
 get '/' do
   @feeds = {}
-  feeds_names = ['politics', 'world', 'economy', 'science', 'sport', 'culture', 'eco']
 
-  feeds_names.each do |feed_name|
+
+  settings.feeds_names.each do |feed_name|
     @feeds[feed_name] = settings.cache.get(feed_name)
   end
 
   erb :index
 end
+
+get '/rubrics/:name/' do
+
+  if settings.feeds_names.include?(params[:name])
+    @feed = settings.cache.get(params[:name])
+    erb :rubric
+  else
+    status 404
+  end
+
+end
+
